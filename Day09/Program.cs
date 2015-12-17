c47adb79a62a12c88d89ba6b724446ad65a160b1
Petr Faltýnek <pfaltynek@gmail.com>
2015-12-17 22:50:19 +0100

    started day 9 solution

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -9,13 +9,19 @@ namespace Day09 {
 		private const string input_path = "./input.txt";
 
 		static void Main(string[] args) {
-			string[] input;
-			string line, rest, result;
-			int index;
-			int sum_chars, sum_code;
+			string[] input, parts;
+			uint distance;
+			string loc1, loc2;
+			Dictionary<string,Dictionary<string, uint>> distances = new Dictionary<string, Dictionary<string, uint>>();
+			List<string> destinations = new List<string>();
+			List<string> route = new List<string>();
+			List<List<string>> valid_routes = new List<List<string>>();
+			int[,] index_map;
+			int max_routes, tmp;
+
 			Console.WriteLine("=== Advent of Code - day 9 ====");
 
-			if (!System.IO.File.Exists(input_path)) {
+			if(!System.IO.File.Exists(input_path)) {
 				Console.WriteLine("input file not found");
 				return;
 			}
@@ -24,17 +30,55 @@ namespace Day09 {
 
 			#region part 1
 
-			sum_code = 0;
-			sum_chars = 0;
+			#region get all distances
 
-			for (int i = 0; i < input.Length; i++) {
-				line = input[i].Trim();
+			for(int i = 0; i < input.Length; i++) {
+				parts = input[i].Trim().Split(new string[]{ " to ", " = " }, StringSplitOptions.RemoveEmptyEntries);
+				if(!parts.Length.Equals(3)) {
+					Console.WriteLine("Invalid direction at line {0}", i + 1);
+					return;
+				}
+				if(!uint.TryParse(parts[2].Trim(), out distance)) {
+					Console.WriteLine("Unable to parse distance at line {0}", i + 1);
+					return;
+				}
 
+				loc1 = parts[0].Trim();
+				loc2 = parts[1].Trim();
 
+				if(!distances.ContainsKey(loc1)) {
+					distances.Add(loc1, new Dictionary<string,uint>());
+				}
+				distances[loc1].Add(loc2, distance);
+
+				if(!distances.ContainsKey(loc2)) {
+					distances.Add(loc2, new Dictionary<string,uint>());
+				}
+				distances[loc2].Add(loc1, distance);
 			}
 
+			#endregion
+
+			#region get all available routes
+
+			foreach (string item in distances.Keys) {
+				destinations.Add(item);
+			}
+
+			tmp = destinations.Count;
+			max_routes = 1;
+			while(tmp > 1) {
+				max_routes = max_routes * tmp;
+				tmp--;
+			}
+
+			index_map = new int[max_routes, destinations.Count];
+
+			#endregion
+
 			Console.WriteLine("--- part 1 ---");
-			Console.WriteLine("Result is {0}", sum_chars - sum_code);
+
+			Console.WriteLine("Result is {0}", 0);
 
 			#endregion
 
@@ -42,8 +86,7 @@ namespace Day09 {
 
 			Console.WriteLine("--- part 2 ---");
 
-
-			Console.WriteLine("Result is {0}", sum_code - sum_chars);
+			Console.WriteLine("Result is {0}", 0);
 
 			#endregion
 		}
