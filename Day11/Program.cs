2ffa1ff8b97c759db28336e3497d8aca48a3c8ac
Petr Faltýnek <pfaltynek@gmail.com>
2015-12-20 21:53:46 +0100

    added solution for day 11

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Day11 {
 	class MainClass {
@@ -23,6 +25,32 @@ namespace Day11 {
 			#region part 1
 
 			Console.WriteLine("--- part 1 ---");
+			/*
+			bool test;
+			test = CheckStraight3Chars("hijklmmn");
+			test = CheckDisabledCharacters("hijklmmn");
+			test = CheckDifferentPairs("hijklmmn");
+
+			test = CheckStraight3Chars("abbceffg");
+			test = CheckDisabledCharacters("abbceffg");
+			test = CheckDifferentPairs("abbceffg");
+
+			test = CheckStraight3Chars("abcdefgh");
+			test = CheckDisabledCharacters("abcdefgh");
+			test = CheckDifferentPairs("abcdefgh");
+
+			test = CheckStraight3Chars("abcdffaa");
+			test = CheckDisabledCharacters("abcdffaa");
+			test = CheckDifferentPairs("abcdffaa");
+
+			test = CheckStraight3Chars("ghijklmn");
+			test = CheckDisabledCharacters("ghijklmn");
+			test = CheckDifferentPairs("ghijklmn");
+
+			test = CheckStraight3Chars("ghjaabcc");
+			test = CheckDisabledCharacters("ghjaabcc");
+			test = CheckDifferentPairs("ghjaabcc");
+			*/
 
 			result = input;
 			do {
@@ -37,29 +65,60 @@ namespace Day11 {
 
 			Console.WriteLine("--- part 2 ---");
 
+			do {
+				IncrementPassword(ref result);
+			} while(!CheckPassword(result));
+
 			Console.WriteLine("Result is {0}", result);
 
 			#endregion
 		}
 
 		private static void IncrementPassword(ref string current_password) {
-			int index = current_password.Length - 1;
 			bool overflow = false;
+			char[] pwd = current_password.ToCharArray();
+			int i = pwd.Length - 1;
+
+			if(i < 0) {
+				return;
+			}
 
 			do {
-				current_password[i]++;
-				if(current_password[i] > 'z') {
-					current_password[i] = 'a';
+				pwd[i]++;
+				if(pwd[i] > 'z') {
+					pwd[i] = 'a';
 					overflow = true;
 				} else {
 					overflow = false;
 				}
-			} while(overflow && (index >= 0));
+				i--;
+			} while(overflow && (i >= 0));
+
+			current_password = new string(pwd);
 		}
 
 		private static bool CheckPassword(string password) {
-			single_pair.Matches();
-			return true;
+			return CheckStraight3Chars(password) && CheckDifferentPairs(password) && CheckDisabledCharacters(password);
+		}
+
+		private static bool CheckDisabledCharacters(string password) {
+			return !disabled_characters.IsMatch(password);
+		}
+
+		private static bool CheckDifferentPairs(string password) {
+			List<string> pairs = new List<string>();
+
+			foreach (Match item in single_pair.Matches(password)) {
+				if(!pairs.Contains(item.Value)) {
+					pairs.Add(item.Value);
+				}
+			}
+
+			return pairs.Count >= 2;
+		}
+
+		private static bool CheckStraight3Chars(string password) {
+			return increasing_straight3character.IsMatch(password);
 		}
 	}
 }
