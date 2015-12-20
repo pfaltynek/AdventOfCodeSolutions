fdfd68e7fb36407f2e6f96b590357173c9a9e6de
Petr Faltýnek <pfaltynek@gmail.com>
2015-12-20 20:57:45 +0100

    added solution for day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace Day10 {
+	class MainClass {
+		private static string input = "1113122113";
+
+		public static void Main(string[] args) {
+			string result = string.Empty;
+
+			Console.WriteLine("=== Advent of Code - day 9 ====");
+
+			#region part 1
+
+			for(int i = 0; i < 40; i++) {
+				
+			}
+
+			Console.WriteLine("--- part 1 ---");
+
+			result = input;
+			for(int i = 0; i < 40; i++) {
+				Console.WriteLine("{0}: {1}", i + 1, result);
+				Console.WriteLine();
+				result = LookAndSay(result);
+			}
+
+			Console.WriteLine("Result is {0}", result.Length);
+
+			#endregion
+
+			#region part 2
+
+			Console.WriteLine("--- part 2 ---");
+
+			for(int i = 40; i < 50; i++) {
+				Console.WriteLine("{0}: {1}", i + 1, result);
+				Console.WriteLine();
+				result = LookAndSay(result);
+			}
+
+			Console.WriteLine("Result is {0}", result.Length);
+
+			#endregion
+		}
+
+		private static string LookAndSay(string input) {
+			string result = string.Empty;
+			char current;
+			int counter;
+
+			if(input.Length > 0) {
+				current = input[0];
+				counter = 1;
+
+				for(int i = 1; i < input.Length; i++) {
+					if(input[i].Equals(current)) {
+						counter++;
+					} else {
+						result += string.Format("{0}{1}", counter, current);
+						current = input[i];
+						counter = 1;
+					}
+				}
+			} else {
+				return string.Empty;
+			}
+
+			result += string.Format("{0}{1}", counter, current);
+
+			return result;
+		}
+	}
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day11 {
+	class MainClass {
+		private static string input = "vzbxkghb";
+		private static Regex disabled_characters = new Regex("[iol]");
+		private static Regex increasing_straight3character = new Regex("(abc|bcd|cde|def|efg|fgh|ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)");
+		private static Regex single_pair = new Regex("([a-z])\\1");
+
+		public static void Main(string[] args) {
+			string result = string.Empty;
+			/*
+			Regex vowels = new Regex("([aeiou].*?){3,}");
+			Regex doubled = new Regex("(.)\\1+");
+			Regex invalid = new Regex("ab|cd|pq|xy");
+			Regex dblpairs = new Regex("(.{2})(.)*?\\1");
+			Regex snglpairs = new Regex("(.)(.)\\1");
+			*/
+
+			Console.WriteLine("=== Advent of Code - day 11 ====");
+
+			#region part 1
+
+			Console.WriteLine("--- part 1 ---");
+
+			result = input;
+			do {
+				IncrementPassword(ref result);
+			} while(!CheckPassword(result));
+
+			Console.WriteLine("Result is {0}", result);
+
+			#endregion
+
+			#region part 2
+
+			Console.WriteLine("--- part 2 ---");
+
+			Console.WriteLine("Result is {0}", result);
+
+			#endregion
+		}
+
+		private static void IncrementPassword(ref string current_password) {
+			int index = current_password.Length - 1;
+			bool overflow = false;
+
+			do {
+				current_password[i]++;
+				if(current_password[i] > 'z') {
+					current_password[i] = 'a';
+					overflow = true;
+				} else {
+					overflow = false;
+				}
+			} while(overflow && (index >= 0));
+		}
+
+		private static bool CheckPassword(string password) {
+			single_pair.Matches();
+			return true;
+		}
+	}
+}
