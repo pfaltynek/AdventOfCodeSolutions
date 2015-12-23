c7e0ac65c2b29b25215d10d7780bc58ece5d866c
Petr Faltýnek <pfaltynek@gmail.com>
2015-12-23 23:26:07 +0100

    added solution for part 1 of day 14

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -19,7 +19,7 @@ namespace Day13 {
 
 			Console.WriteLine("=== Advent of Code - day 13 ====");
 
-			if (!System.IO.File.Exists(input_path)) {
+			if(!System.IO.File.Exists(input_path)) {
 				Console.WriteLine("input file not found");
 				return;
 			}
@@ -30,15 +30,15 @@ namespace Day13 {
 
 			#region get all scores
 
-			for (int i = 0; i < input.Length; i++) {
+			for(int i = 0; i < input.Length; i++) {
 				line = input[i].Replace("lose ", "-");
 				line = line.Replace("gain ", "+");
-				parts = line.Trim().Split(new string[] { " happiness units by sitting next to ", " would ", "."}, StringSplitOptions.RemoveEmptyEntries);
-				if (!parts.Length.Equals(3)) {
+				parts = line.Trim().Split(new string[] { " happiness units by sitting next to ", " would ", "." }, StringSplitOptions.RemoveEmptyEntries);
+				if(!parts.Length.Equals(3)) {
 					Console.WriteLine("Invalid info at line {0}", i + 1);
 					return;
 				}
-				if (!int.TryParse(parts[1].Trim(), out happines)) {
+				if(!int.TryParse(parts[1].Trim(), out happines)) {
 					Console.WriteLine("Unable to parse distance at line {0}", i + 1);
 					return;
 				}
@@ -46,7 +46,7 @@ namespace Day13 {
 				person1 = parts[0].Trim();
 				person2 = parts[2].Trim();
 
-				if (!happines_units.ContainsKey(person1)) {
+				if(!happines_units.ContainsKey(person1)) {
 					happines_units.Add(person1, new Dictionary<string, int>());
 				}
 				happines_units[person1].Add(person2, happines);
@@ -55,6 +55,8 @@ namespace Day13 {
 			}
 
 			#endregion
+
+			Console.WriteLine("--- part 1 ---");
 
 			#region get all available routes and calculate their happines
 
@@ -67,14 +69,20 @@ namespace Day13 {
 			FillAllSittingOrders(persons, new List<string>(), ref available_orders, ref happines_units);
 
 			foreach (List<string> item in available_orders.Keys) {
-				if (available_orders[item] > max_happines) {
+				if(available_orders[item] > max_happines) {
 					max_happines = available_orders[item];
 				}
 			}
 
 			#endregion
 
-			Console.WriteLine("--- part 1 ---");
+			Console.WriteLine("Result is {0}", max_happines);
+
+			#endregion
+
+			#region part 2
+
+			Console.WriteLine("--- part 2 ---");
 
 			foreach (string item in happines_units.Keys) {
 				happines_units[item].Add("Me", 0);
@@ -89,18 +97,10 @@ namespace Day13 {
 			FillAllSittingOrders(persons, new List<string>(), ref available_orders, ref happines_units);
 
 			foreach (List<string> item in available_orders.Keys) {
-				if (available_orders[item] > max_happines2) {
+				if(available_orders[item] > max_happines2) {
 					max_happines2 = available_orders[item];
 				}
 			}
-
-			Console.WriteLine("Result is {0}", max_happines);
-
-			#endregion
-
-			#region part 2
-
-			Console.WriteLine("--- part 2 ---");
 
 			Console.WriteLine("Result is {0}", max_happines2);
 
@@ -108,13 +108,12 @@ namespace Day13 {
 		}
 
 		private static void FillAllSittingOrders(List<string> free_persons, List<string> sitting_order, ref Dictionary<List<string>, int> available_orders, ref Dictionary<string, Dictionary<string, int>> units) {
-			if (free_persons.Count.Equals(1)) {
+			if(free_persons.Count.Equals(1)) {
 				List<string> next_sitting_order = new List<string>(sitting_order);
 				next_sitting_order.Add(free_persons[0]);
 				available_orders.Add(next_sitting_order, CalculateSittingOrderHappines(next_sitting_order, ref units));
-			}
-			else {
-				for (int i = 0; i < free_persons.Count; i++) {
+			} else {
+				for(int i = 0; i < free_persons.Count; i++) {
 					string item = free_persons[i];
 					List<string> next_sitting_order = new List<string>(sitting_order);
 					List<string> next_free_persons = new List<string>(free_persons);
@@ -128,18 +127,16 @@ namespace Day13 {
 		private static int CalculateSittingOrderHappines(List<string> sitting_order, ref Dictionary<string, Dictionary<string, int>> units) {
 			int result = 0;
 
-			for (int i = 0; i < sitting_order.Count; i++) {
+			for(int i = 0; i < sitting_order.Count; i++) {
 				string left, right;
 
-				if (i.Equals(0)) {
+				if(i.Equals(0)) {
 					left = sitting_order[sitting_order.Count - 1];
 					right = sitting_order[i + 1];
-				}
-				else if (i.Equals(sitting_order.Count - 1)) {
+				} else if(i.Equals(sitting_order.Count - 1)) {
 					left = sitting_order[i - 1];
 					right = sitting_order[0];
-				}
-				else {
+				} else {
 					left = sitting_order[i - 1];
 					right = sitting_order[i + 1];
 				}
@@ -149,41 +146,5 @@ namespace Day13 {
 			return result;
 		}
 
-		private static void FindAllAvailableRoutes(
-			List<string> current_route,
-			List<string> available_destinations,
-			Dictionary<string, Dictionary<string, uint>> distances,
-			uint current_distance,
-			ref Dictionary<string, uint> available_routes) {
-
-			if (current_route.Count.Equals(0)) {
-				for (int i = 0; i < available_destinations.Count; i++) {
-					List<string> next_destinations = new List<string>(available_destinations);
-					next_destinations.Remove(available_destinations[i]);
-					List<string> updated_route = new List<string>(current_route);
-					updated_route.Add(available_destinations[i]);
-					FindAllAvailableRoutes(updated_route, next_destinations, distances, current_distance, ref available_routes);
-				}
-			}
-			else {
-				string current_dest = current_route[current_route.Count - 1];
-				for (int i = 0; i < available_destinations.Count; i++) {
-					if (distances[current_dest].ContainsKey(available_destinations[i])) {
-						uint next_distance = current_distance + distances[current_dest][available_destinations[i]];
-						if (available_destinations.Count.Equals(1)) {
-							current_route.Add(available_destinations[i]);
-							available_routes.Add(string.Join("->", current_route), next_distance);
-						}
-						else {
-							List<string> next_destinations = new List<string>(available_destinations);
-							next_destinations.Remove(available_destinations[i]);
-							List<string> updated_route = new List<string>(current_route);
-							updated_route.Add(available_destinations[i]);
-							FindAllAvailableRoutes(updated_route, next_destinations, distances, next_distance, ref available_routes);
-						}
-					}
-				}
-			}
-		}
 	}
 }
---
+++ Day14/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Security.Authentication;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace Day14 {
+	class MainClass {
+		private const string input_path = "./input.txt";
+		private const int duration_part1 = 2503;
+
+		private class reindeer_speed {
+			public string Name;
+			public int Speed;
+			public int SpeedDuration;
+			public int RestTime;
+
+			public reindeer_speed(string name, int speed, int speed_duration, int rest_time) {
+				Name = name;
+				Speed = speed;
+				SpeedDuration = speed_duration;
+				RestTime = rest_time;
+			}
+		}
+
+		public static void Main(string[] args) {
+			string[] input, parts;
+			int max_distance = int.MinValue;
+			Dictionary<reindeer_speed, int> distances = new Dictionary<reindeer_speed, int>();
+
+			Console.WriteLine("=== Advent of Code - day 14 ====");
+
+			if(!System.IO.File.Exists(input_path)) {
+				Console.WriteLine("input file not found");
+				return;
+			}
+
+			input = System.IO.File.ReadAllLines(input_path);
+
+			#region get all speeds
+
+			for(int i = 0; i < input.Length; i++) {
+				int speed, time1, time2, distance;
+				reindeer_speed info;
+
+				parts = input[i].Split(new string[]{ " can fly ", " km/s for ", " seconds, but then must rest for ", " seconds." }, StringSplitOptions.RemoveEmptyEntries);
+				if(!parts.Length.Equals(4)) {
+					throw new InvalidDataException(string.Format("Invalid reindeer info at line {0}", i + 1));
+				}
+				if(!int.TryParse(parts[1], out speed)) {
+					throw new InvalidDataException(string.Format("Invalid speed info at line {0}", i + 1));
+				}
+				if(!int.TryParse(parts[2], out time1)) {
+					throw new InvalidDataException(string.Format("Invalid speed duration at line {0}", i + 1));
+				}
+				if(!int.TryParse(parts[3], out time2)) {
+					throw new InvalidDataException(string.Format("Invalid speed rest time at line {0}", i + 1));
+				}
+				info = new reindeer_speed(parts[0], speed, time1, time2);
+				distance = CalculateDistance(info, duration_part1);
+				if(max_distance < distance) {
+					max_distance = distance;
+				}
+				distances.Add(info, distance);
+			}
+
+			#endregion
+
+			#region part 1
+
+			Console.WriteLine("--- part 1 ---");
+
+
+			Console.WriteLine("Result is {0}", max_distance);
+
+			#endregion
+
+			#region part 2
+
+			Console.WriteLine("--- part 2 ---");
+
+			Console.WriteLine("Result is {0}", max_distance);
+
+			#endregion
+		}
+
+		private static int CalculateDistance(reindeer_speed info, int time) {
+			int cycle_time = info.SpeedDuration + info.RestTime;
+			int cycles = time / cycle_time;
+			int rest_time = time % cycle_time;
+			int result = cycles * info.SpeedDuration * info.Speed;
+			if(rest_time >= info.SpeedDuration) {
+				result += (info.SpeedDuration * info.Speed);
+			} else {
+				result += (rest_time * info.Speed);
+			}
+			return result;
+		}
+	}
+}
